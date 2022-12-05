fc6a19f2b7de8f2cc82eb8591ea72f071dcef212
ppedvLukasK <lukask@ppedv.de>
2022-12-05 14:50:10 +0100

    M004

---
+++ M004.py
@@ -0,0 +1,82 @@
+# Schleifen
+# Wenn man etwas mehrmals hintereinander machen möchte
+
+# While Schleife
+# Läuft solange die Bedingung True ist
+# Zähler sollte dabei sein (in den meisten Fällen)
+i = 0
+while i < 10:  # Bedingung, Schleife läuft bis die Bedingung false ist
+	print(i)
+	i += 1  # Wichtig: Zähler erhöhen
+else:  # Wird nach der Schleife ausgeführt, wenn die Schleife fehlerfrei ausgeführt wurde (ohne break)
+	print("Schleife fertig")
+
+while True:
+	print("Text")
+	i += 1
+	if i == 500:
+		break  # Beende die Schleife, sollte immer mit einer if/elif/else kombiniert werden
+else:  # Wird nie ausgeführt
+	print("Schleife fertig")
+
+# for Schleife
+# Verhält sich wie foreach in anderen Sprachen
+# Wird verwendet um über eine Collection zu iterieren (List, Tuple, String, Range, ...)
+# Syntax:
+# for <Variablenname> in <Liste>:
+liste = ["Ich", "bin", "eine", "Liste"]
+
+for wort in liste:  # wort: Variablenname für das derzeitige Wort
+	print(wort)
+	print("Ein Wort wurde geschrieben")  # Mehrere Codezeilen in einer Schleife ausführen
+
+# for Schleife mit Zähler
+for i in range(0, 10):  # Eine Schleife die von 0 bis 9 geht
+	print(i)
+
+text = "Ein Text"
+for zeichen in text:  # Mit for-Schleife über String gehen
+	print(zeichen)
+
+# break und continue
+# break: Die Schleife beenden und mit dem Code danach weitermachen
+# continue: Zum Schleifenkopf zurückspringen und den Code danach nicht ausführen
+for i in range(0, 10):
+	print(i)
+	if i == 5:
+		break  # Wenn i == 5, beende die Schleife
+
+print("Test")
+
+for i in range(0, 10):
+	if i == 5:
+		continue  # Wenn i == 5, gehe zum Schleifenzurück und überspringe den Code danach
+	print(i)
+
+# else am Ende einer Schleife
+# Geht auch bei for
+for i in range(0, 10):
+	print(i)
+else:
+	print("Schleife fertig")
+
+# fstring
+# Code in einen String schreiben
+# Syntax:
+# f"Text1 Text2 {<Code>}"
+a = 5
+str2 = "Text1 Text2 " + a.__str__()  # ohne fstring
+fstring = f"Text1 Text2 {a}"  # einfach a einbinden und wird automatisch konvertiert
+print(fstring)
+
+# Ich möchte pro Schleifendurchlauf immer i, i^2 und die Gleichung ausgeben
+for i in range(0, 10):
+	# Ohne fstring
+	print("Die Zahl ist " + i.__str__())
+	print("Die Quadrierte Zahl ist " + (i*i).__str__())
+	print(i.__str__() + "^2=" + (i*i).__str__())
+
+	# Mit fstring
+	print(f"Die Zahl ist {i}")
+	print(f"Die Quadrierte Zahl ist {i*i}")
+	print(f"{i}^2={i*i}")
